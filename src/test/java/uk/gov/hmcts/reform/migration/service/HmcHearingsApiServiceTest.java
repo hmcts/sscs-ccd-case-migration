aa767ca4ebc9b7812269e90839d5af88152697f2
hashimalisolirius <HashimAli@HMCTS.NET>
2024-11-26 14:17:48 +0000

    test for hmc service

---
+++ src/test/java/uk/gov/hmcts/reform/migration/service/HmcHearingsApiServiceTest.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.reform.migration.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.domain.hmc.HearingsGetResponse;
+import uk.gov.hmcts.reform.domain.hmc.HmcStatus;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class HmcHearingsApiServiceTest {
+
+    @Mock
+    private IdamService idamService;
+
+    @Mock
+    private HmcHearingsApi hmcHearingsApi;
+
+    @InjectMocks
+    private HmcHearingsApiService hmcHearingsApiService;
+
+    @Test
+    void shouldReturnResponse() {
+        HearingsGetResponse hmcResponse = HearingsGetResponse.builder().build();
+        when(idamService.getIdamTokens()).thenReturn(
+            IdamTokens.builder().serviceAuthorization("test").idamOauth2Token("test").build());
+        when(hmcHearingsApi.getHearingsRequest(any(),any(), any(),any())).thenReturn(hmcResponse);
+        HearingsGetResponse response = hmcHearingsApiService.getHearingsRequest("1234", HmcStatus.COMPLETED);
+        assert (response).equals(hmcResponse);
+    }
+}
