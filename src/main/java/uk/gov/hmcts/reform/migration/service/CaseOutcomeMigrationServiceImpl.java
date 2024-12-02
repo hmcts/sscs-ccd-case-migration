bee2ee6794513536dd2d1bfdcee94718c2376feb
Gemma Talbot <gemma.talbot@solirius.com>
2024-12-02 17:29:03 +0000

    add comments for updating pr

--- src/main/java/uk/gov/hmcts/reform/migration/service/CaseOutcomeMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/CaseOutcomeMigrationServiceImpl.java
@@ -27,6 +27,7 @@ import static java.util.Objects.nonNull;
 @Slf4j
 @ConditionalOnProperty(value = "migration.hearingOutcomesMigration.enabled", havingValue = "true")
 public class CaseOutcomeMigrationServiceImpl  implements DataMigrationService<Map<String, Object>> {
+    //CHANGE EVENT ID TO caseOutcomeMigration ONCE TRIBS PR #4193 PUSHED TO MASTER
     static final String EVENT_ID = "caseManagementLocationMigration";
     // ASK UMA TO CHECK WHAT EVENT SUMMARY AND EVENT DESCRIPTION SHOULD BE
     static final String EVENT_SUMMARY = "Migrate Case Outcome fields to Hearing Outcome";
