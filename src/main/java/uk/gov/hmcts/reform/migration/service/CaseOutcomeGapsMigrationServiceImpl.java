563fbe071382c2461e25869953b4e633a5c50d5b
Gemma Talbot <gemma.talbot@solirius.com>
2025-01-20 13:24:19 +0000

    update summary and description

--- src/main/java/uk/gov/hmcts/reform/migration/service/CaseOutcomeGapsMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/CaseOutcomeGapsMigrationServiceImpl.java
@@ -19,8 +19,8 @@ import static java.util.Objects.nonNull;
 @ConditionalOnProperty(value = "migration.caseOutcomeGapsMigration.enabled", havingValue = "true")
 public class CaseOutcomeGapsMigrationServiceImpl  implements DataMigrationService<Map<String, Object>> {
     static final String EVENT_ID = "removeCaseOutcome";
-    static final String EVENT_SUMMARY = "Remove old case outcome";
-    static final String EVENT_DESCRIPTION = "Remove old case outcome";
+    static final String EVENT_SUMMARY = "Outcome tab removed as it is Gaps case";
+    static final String EVENT_DESCRIPTION = "Outcome tab removed as it is Gaps case";
 
     public Predicate<CaseDetails> accepts() {
         return Objects::nonNull;
