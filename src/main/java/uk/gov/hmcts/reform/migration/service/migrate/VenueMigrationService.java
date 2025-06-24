501dbc2fb9b94aea8bd0eb7e3572bc29014a891b
taleb <tech@benouaer.com>
2025-06-24 11:03:30 +0100

    SSCSCI-1935: fix checkstyle error

--- src/main/java/uk/gov/hmcts/reform/migration/service/migrate/VenueMigrationService.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/migrate/VenueMigrationService.java
@@ -23,8 +23,8 @@ public class VenueMigrationService extends CaseMigrationProcessor {
 
     static final String VENUE_MIGRATION_EVENT_ID = "migrateCase";
     static final String VENUE_MIGRATION_EVENT_SUMMARY = "Migrate processing venue for Child support cases";
-    static final String VENUE_MIGRATION_EVENT_DESCRIPTION = "Migrate processing venue for Child support cases," +
-        "after Airlookup update replaced Bradford, Grimsby, Scarborough and York with Leeds/Hull";
+    static final String VENUE_MIGRATION_EVENT_DESCRIPTION = "Migrate processing venue for Child support cases,"
+        + "after Airlookup update replaced Bradford, Grimsby, Scarborough and York with Leeds/Hull";
     static final String PROCESSING_VENUE_FIELD = "processingVenue";
     static final String FAILURE_MSG = "Skipping Case (%s) for migration because no venue was found";
 
