16b40e1d545c6531907f6069eaf838b6349a401d
Katalin Kovacs <Katalin.Kovacs@HMCTS.NET>
2025-08-01 10:08:57 +0100

    Added service tests

--- src/test/java/uk/gov/hmcts/reform/migration/service/HearingOutcomeServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/migration/service/HearingOutcomeServiceTest.java
@@ -1,8 +1,12 @@
 package uk.gov.hmcts.reform.migration.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.domain.hmc.CaseHearing;
@@ -40,8 +44,9 @@ public class HearingOutcomeServiceTest {
         hearingOutcomeService = new HearingOutcomeService(venueService);
     }
 
-    @Test
-    public void shouldReturnEmptyHearingOutcome() {
+    @ParameterizedTest
+    @ValueSource(strings = {"caseOutcome", "outcome"})
+    public void shouldReturnEmptyHearingOutcome(String outcomeSource) {
 
         when(venueService.getVenueDetailsForActiveVenueByEpimsId(isNull()))
             .thenReturn(null);
@@ -61,7 +66,7 @@ public class HearingOutcomeServiceTest {
         ).build();
 
         var hearingOutcomes = hearingOutcomeService.mapHmcHearingToHearingOutcome(
-            hmcHearing, buildCaseDataMap(caseData), "caseOutcome");
+            hmcHearing, buildCaseDataMap(caseData), outcomeSource);
 
         assertEquals(expectedHearingOutcome, hearingOutcomes.getFirst());
     }
@@ -103,6 +108,55 @@ public class HearingOutcomeServiceTest {
 
         var hearingOutcomes = hearingOutcomeService.mapHmcHearingToHearingOutcome(
             hmcHearing, buildCaseDataMap(caseData), "caseOutcome");
+
+        assertEquals(expectedHearingOutcome, hearingOutcomes.getFirst());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "referenceRevisedFavourAppellant, 21",
+        "referenceRevisedAgainstAppellant, 22",
+        "decisionUpheld, 9",
+        "decisionRevisedAgainstAppellant, 8",
+        "decisionReserved, 63",
+        "decisionInFavourOfAppellant, 10",
+        "disablementIncreasedNoBenefitAwarded, 39"
+    })
+    @DisplayName("Should create a Hearing Outcome and map the correct Outcome value to Case Outcome value")
+    void shouldReturnNewHearingOutcomeWithCorrectOutcomeMApping(String outcome, String caseOutcome) {
+        when(venueService.getVenueDetailsForActiveVenueByEpimsId(eq("epimsId")))
+            .thenReturn(VenueDetails.builder()
+                            .venAddressLine1("Windsor Castle")
+                            .venAddressTown("Windsor")
+                            .epimsId("epimsId")
+                            .build());
+        var hmcHearing = CaseHearing.builder()
+            .hearingChannels(List.of(HearingChannel.FACE_TO_FACE))
+            .hearingDaySchedule(
+                List.of(HearingDaySchedule.builder()
+                            .hearingStartDateTime(LocalDateTime.parse("2024-06-30T10:00:00"))
+                            .hearingEndDateTime(LocalDateTime.parse("2024-06-30T11:00:00"))
+                            .hearingVenueEpimsId("epimsId").build()))
+            .hearingId(123456789L).build();
+        var caseData = SscsCaseData.builder()
+            .outcome(outcome)
+            .build();
+        var expectedHearingOutcome = HearingOutcome.builder().value(
+            HearingOutcomeDetails.builder()
+                .completedHearingId("123456789")
+                .hearingStartDateTime(LocalDateTime.parse("2024-06-30T11:00:00"))
+                .hearingEndDateTime(LocalDateTime.parse("2024-06-30T12:00:00"))
+                .hearingOutcomeId(caseOutcome)
+                .hearingChannelId(HearingChannel.FACE_TO_FACE)
+                .venue(Venue.builder().address(
+                    Address.builder().line1("Windsor Castle").town("Windsor").build()
+                ).build())
+                .epimsId("epimsId")
+                .build()
+        ).build();
+
+        var hearingOutcomes = hearingOutcomeService.mapHmcHearingToHearingOutcome(
+            hmcHearing, buildCaseDataMap(caseData), "outcome");
 
         assertEquals(expectedHearingOutcome, hearingOutcomes.getFirst());
     }
