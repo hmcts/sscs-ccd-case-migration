fef2b384f3b1c7ebc59ff4b30a2114cc26ba7156
hashimalisolirius <HashimAli@HMCTS.NET>
2026-01-28 12:21:13 +0000

    Add ReadyToListMigration

---
+++ src/main/java/uk/gov/hmcts/reform/migration/service/migrate/ReadyToListMigration.java
@@ -0,0 +1,102 @@
+package uk.gov.hmcts.reform.migration.service.migrate;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.migration.service.CaseMigrationProcessor;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.service.UpdateCcdCaseService.UpdateResult;
+
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Objects;
+
+import static java.lang.String.format;
+import static uk.gov.hmcts.reform.migration.repository.EncodedStringCaseList.findCases;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.State.RESPONSE_RECEIVED;
+
+@Service
+@Slf4j
+@ConditionalOnProperty(value = "migration.readytolist.enabled", havingValue = "true")
+public class ReadyToListMigration extends CaseMigrationProcessor {
+
+    static final String READY_TO_LIST_MIGRATION_EVENT_ID = "readyToList";
+    static final String READY_TO_LIST_MIGRATION_EVENT_SUMMARY = "Move case state to ready to list";
+    static final String READY_TO_LIST_MIGRATION_EVENT_DESCRIPTION = "";
+    static final String CALLBACK_WARNING_FIELD = "ignoreCallbackWarnings";
+    static final String FAILURE_MSG = "Skipping Case (%s) for migration due to incorrect state: (%s)";
+
+    private final String encodedDataStringA;
+    private final String encodedDataStringB;
+    private final String encodedDataStringC;
+    private final String encodedDataStringD;
+    private final String encodedDataStringE;
+
+
+    public ReadyToListMigration(@Value("${migration.readytolist.encoded-string-a}")
+                                 String encodedDataStringA,
+                                @Value("${migration.readytolist.encoded-string-b}")
+                                 String encodedDataStringB,
+                                @Value("${migration.readytolist.encoded-string-c}")
+                                 String encodedDataStringC,
+                                @Value("${migration.readyToList.encoded-string-d}")
+                                 String encodedDataStringD,
+                                @Value("${migration.readyToList.encoded-string-e}")
+                                 String encodedDataStringE) {
+        this.encodedDataStringA = encodedDataStringA;
+        this.encodedDataStringB = encodedDataStringB;
+        this.encodedDataStringC = encodedDataStringC;
+        this.encodedDataStringD = encodedDataStringD;
+        this.encodedDataStringE = encodedDataStringE;
+    }
+
+    @Override
+    public List<SscsCaseDetails> fetchCasesToMigrate() {
+        return findCases(getEncodedString());
+    }
+
+    @Override
+    public UpdateResult migrate(CaseDetails caseDetails) {
+        if (Objects.equals(caseDetails.getState(), RESPONSE_RECEIVED.toString())) {
+            caseDetails.getData().put(CALLBACK_WARNING_FIELD, "Yes");
+            log.info("Setting ignoreCallbackWarning field to (Yes) for case {}", caseDetails.getId());
+            return new UpdateResult(getEventSummary(), getEventDescription());
+
+        } else {
+            String failureMsg = format(FAILURE_MSG, caseDetails.getId(), caseDetails.getState());
+            log.error(failureMsg);
+            throw new RuntimeException(failureMsg);
+        }
+
+    }
+
+    @Override
+    public String getEventId() {
+        return READY_TO_LIST_MIGRATION_EVENT_ID;
+    }
+
+    @Override
+    public String getEventDescription() {
+        return READY_TO_LIST_MIGRATION_EVENT_DESCRIPTION;
+    }
+
+    @Override
+    public String getEventSummary() {
+        return READY_TO_LIST_MIGRATION_EVENT_SUMMARY;
+    }
+
+    private String getEncodedString() {
+        int time = LocalTime.now(ZoneId.of("Europe/London")).getHour();
+        return switch (time) {
+            case 22 ->  encodedDataStringA;
+            case 23 ->  encodedDataStringB;
+            case 0 -> encodedDataStringC;
+            case 1 -> encodedDataStringD;
+            case 2 -> encodedDataStringE;
+            default -> throw new RuntimeException("Migration job not configured to run at " + time);
+        };
+    }
+}
