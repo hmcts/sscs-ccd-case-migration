05cd39ee381efb7c5bfae16b0436e8ee4e4520f1
Hashim-Ali <140517911+hashimalisolirius@users.noreply.github.com>
2026-01-29 13:02:10 +0000

    SSCSCI-2335: update secret retrieval (#130)
    * update secret retrieval
    
    * update test
    
    * remove csv

--- src/test/java/uk/gov/hmcts/reform/migration/service/migrate/ReadyToListMigrationTest.java
+++ src/test/java/uk/gov/hmcts/reform/migration/service/migrate/ReadyToListMigrationTest.java
@@ -3,19 +3,17 @@ package uk.gov.hmcts.reform.migration.service.migrate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 
-import java.time.Clock;
-import java.time.Instant;
-import java.time.ZoneId;
 import java.util.HashMap;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static uk.gov.hmcts.reform.migration.repository.EncodedStringCaseListTest.ENCODED_CASE_ID;
+import static uk.gov.hmcts.reform.migration.repository.EncodedStringCaseListTest.ENCODED_STRING;
 import static uk.gov.hmcts.reform.migration.service.migrate.ReadyToListMigration.CALLBACK_WARNING_FIELD;
 import static uk.gov.hmcts.reform.migration.service.migrate.ReadyToListMigration.READY_TO_LIST_MIGRATION_EVENT_DESCRIPTION;
 import static uk.gov.hmcts.reform.migration.service.migrate.ReadyToListMigration.READY_TO_LIST_MIGRATION_EVENT_ID;
@@ -26,22 +24,11 @@ import static uk.gov.hmcts.reform.sscs.ccd.domain.State.RESPONSE_RECEIVED;
 class ReadyToListMigrationTest {
 
     private ReadyToListMigration underTest;
-    private static final ZoneId LONDON = ZoneId.of("Europe/London");
-
 
     @BeforeEach
     void setUp() {
         underTest = new ReadyToListMigration(
-            "encodedA",
-            "encodedB",
-            "encodedC",
-            "encodedD",
-            "encodedE",
-            "encodedF",
-            "encodedG",
-            "encodedH",
-            "encodedI",
-            "encodedJ"
+            ENCODED_STRING
         );
     }
 
@@ -93,34 +80,13 @@ class ReadyToListMigrationTest {
         assertThat(underTest.getEventDescription()).isEqualTo(READY_TO_LIST_MIGRATION_EVENT_DESCRIPTION);
     }
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/ready_to_list_migration_hours.csv", numLinesToSkip = 1)
-    void shouldSelectCorrectEncodedStringBasedOnHour(int hour, String expected) {
-        ReadyToListMigration migration = new ReadyToListMigration(
-            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"
-        );
-
-        Clock fixedClock = Clock.fixed(
-            Instant.parse(String.format("2024-01-01T%02d:00:00Z", hour)),
-            LONDON
-        );
-        ReflectionTestUtils.setField(migration, "clock", fixedClock);
-        String result = migration.getEncodedString();
-        assertThat(result).isEqualTo(expected);
-    }
-
     @Test
-    void getEncodedString_shouldThrowOutsideConfiguredHours() {
-        ReadyToListMigration migration = new ReadyToListMigration(
-            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"
-        );
+    void shouldFetchCasesToMigrate() {
+        var migrationCase = SscsCaseDetails.builder().id(ENCODED_CASE_ID).jurisdiction("SSCS").build();
+        List<SscsCaseDetails> migrationCases = underTest.fetchCasesToMigrate();
 
-        Clock fixedClock = Clock.fixed(Instant.parse("2024-01-01T05:00:00Z"), LONDON);
-        ReflectionTestUtils.setField(migration, "clock", fixedClock);
-
-        RuntimeException ex = assertThrows(IllegalStateException.class, migration::getEncodedString);
-        assertThat(ex.getMessage()).contains("Migration job not configured to run at 5");
+        assertThat(migrationCases).hasSize(1);
+        assertThat(migrationCases).contains(migrationCase);
     }
-
 
 }
