74ab5cf63dcc67c496a08676d872ddc11b00af7f
Gemma Talbot <gemma.talbot@solirius.com>
2026-05-20 17:21:38 +0100

    checkstyle

--- src/test/java/uk/gov/hmcts/reform/migration/service/migrate/VenueMigrationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/migration/service/migrate/VenueMigrationServiceTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.migration.service.migrate;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
